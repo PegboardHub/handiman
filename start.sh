8e8ea0f6a2e6a683966879c8c4d67770f7fc3ea9
Elran Shefer <elran777@gmail.com>
2021-01-09 00:36:19 +0200

    initial commit

---
+++ start.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/git/pegboardhub/
+cd ~/git/pegboardhub/
+git clone git@github.com:PegboardHub/handyman.git
+git clone git@github.com:PegboardHub/pegboard.git
+git clone git@github.com:PegboardHub/concrete.git
