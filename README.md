48e5abbead6ea61d7507af7e80a692e2be28d8fe
Elran Shefer <elran777@gmail.com>
2021-01-09 00:54:52 +0200

    Fixed setup file

---
+++ README.md
@@ -0,0 +1,8 @@
+# Handyman repo
+## Dev utilities for the Pegboard application
+
+
+start with:
+```bash
+curl https://raw.githubusercontent.com/PegboardHub/handyman/master/setup.sh | bash
+```
--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-bash ./clone.sh
+mkdir ./pegboardhub/
+cd ./pegboardhub/
+git clone git@github.com:PegboardHub/handyman.git
+git clone git@github.com:PegboardHub/pegboard.git
+git clone git@github.com:PegboardHub/concrete.git
+
+get_abs_path() {
+  # $1 : relative filename
+  echo "$(cd "$(dirname ".")" && pwd)/"
+}
+
+PEGBOARD_PATH=$(get_abs_path)
+echo "$PEGBOARD_PATH" >> ~/.aliases
+
 source ~/.aliases
 cd "$PEGBOARD_PATH/handyman/"
 fastinstall
