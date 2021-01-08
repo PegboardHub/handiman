30ea173082e129a7833d554a68768da7ec3d4599
Elran Shefer <elran777@gmail.com>
2021-01-09 01:01:51 +0200

    Fixed setup file

--- setup.sh
+++ setup.sh
@@ -6,11 +6,10 @@ git clone git@github.com:PegboardHub/pegboard.git
 git clone git@github.com:PegboardHub/concrete.git
 
 get_abs_path() {
-  # $1 : relative filename
   echo "$(cd "$(dirname ".")" && pwd)/"
 }
 
-PEGBOARD_PATH=$(get_abs_path)
+PEGBOARD_PATH=$()
 grep -qxF 'PEGBOARD_PATH' ~/.aliases || echo "alias PEGBOARD_PATH=\"$PEGBOARD_PATH\"" >> ~/.aliases
 
 source ~/.aliases
