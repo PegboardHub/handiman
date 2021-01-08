ef2f6cd695442dd905658bac73e13e841bc74b8e
Elran Shefer <elran777@gmail.com>
2021-01-09 00:52:38 +0200

    Fixed setup file

--- assets/clone.sh
+++ assets/clone.sh
@@ -1,5 +1,13 @@
-mkdir -p ~/git/pegboardhub/
-cd ~/git/pegboardhub/
+mkdir ./pegboardhub/
+cd ./pegboardhub/
 git clone git@github.com:PegboardHub/handyman.git
 git clone git@github.com:PegboardHub/pegboard.git
 git clone git@github.com:PegboardHub/concrete.git
+
+get_abs_path() {
+  # $1 : relative filename
+  echo "$(cd "$(dirname ".")" && pwd)/"
+}
+
+PEGBOARD_PATH=$(get_abs_path)
+echo "$PEGBOARD_PATH" >> ~/.aliases
